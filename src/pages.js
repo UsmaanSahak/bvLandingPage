4995c36725e32b4a43cce04b13c31ca0affdfcac
null <null>
2019-03-26 20:13:27 +0000

    v2

--- src/pages.js
+++ src/pages.js
@@ -33,36 +33,63 @@ class WelcomeText extends React.Component {
 	render() {
 		return (
 		<div id="WelcomeText">
-		<p>Hello, this is my page.</p>
 		<p>+1 (888) 111-9999</p>
 		<p>myName@domain.com</p>
 		</div>
 		);
 	}
 }
+class MLink extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			hover:false,
+			clicked:false
+		}
+	};
+	clicked() {
+		this.setState({clicked:true});
+		window.location=this.props.link;
+	}
+	
+	mouseOver() {
+		this.setState({hover:true});
+	}
+	mouseLeave() {
+		this.setState({hover:false});
+	}
+	render() {
+		let color = this.state.hover ? "#777788" : "#aaaa00";
+		let boxShadow = this.state.clicked ? "1px 1px 10px #888" : "0 0 0 0 #888";
+		var style = {
+			marginBottom:"2px",
+			fontSize:"13px",
+			color:color,
+			width:"86px",
+			cursor: "pointer",
+			boxShadow: boxShadow
+		};
 
-class TechStack extends React.Component {
-	render() {
 		return (
-		<div id="TechStack">
-		<p> My tech stack is as follows: </p>
-		<br/><br/>
-		<img id="StackImg" className="expandable" width="300px" src="src/explain.png"/>
+		<p onMouseOver={() => this.mouseOver()} onMouseLeave={() => this.mouseLeave()} onClick={() => this.clicked()} style={style}>{this.props.content}</p>
+		);
+
+	}
+	
+}
+class Media extends React.Component {
+		render() {
+
+		return (
+		<div>
+		<MLink content="Linkedin" link="https://linkedin.com"/>
+		<MLink content="Github" link="https://github.com"/>
+		<MLink content="Facebook" link="https://facebook.com"/>
 		</div>
 		);
 	}
 }
-class HardStack extends React.Component {
-	render() {
-		return (
-		<div id="HardStack">
-		<p> This is a schematic template I have created. </p>
-		<br/><br/>
-		<img id="HardImg" className="expandable" width="300px" src="src/bvhardware.png"/>
-		</div>
-		);
-	}
-}
+
 class Bullets extends React.Component {
 	points() {
 		//Make 1 div float left for topic.
@@ -77,6 +104,7 @@ class Bullets extends React.Component {
 	render() {
 		var topic = {
 			float: "left",
+			color:"black",
 			width: "80px",
 			marginRight: "50px"
 		};
@@ -100,57 +128,93 @@ class Bullets extends React.Component {
 		);
 	}
 }
-class Body extends React.Component {
+class Brief extends React.Component {
 	render() {
 		return (
-		<div>
 		<div id="brief">
 		<h2>What are your needs?</h2>
 		<h4>Common Web Formats:</h4>
 		
-		<Bullets topic="List Format" points={["Good for websites that can comfortably fit all of their content on one page.","Easy to use or add to.","Works predictably on both desktop and mobile."]}/>
+		<Bullets topic="List Format" points={["Good for websites that can comfortably fit all of their content on one page. ","Easy to use or add to. ","Works predictably on both desktop and mobile."]}/>
 		<Bullets topic="Index Format" points={["Good for abstract websites whose main purpose is to point to more specific pages or services."]}/>
-		<Bullets topic="Aggregate Format" points={["Good for websites tha have a lot of content, and wants to display a portion of it while providding an index to point to the remianing content.","Between list and index."]}/>
+		<Bullets topic="Aggregate Format" points={["Good for websites tha have a lot of content, and wants to display a portion of it while providing an index to point to the remianing content. ","Between list and index."]}/>
 		<Bullets topic="Search Format" points={["Good for websites that either have a massive amount of content or want to direct their user based off of input."]}/>
 		<Bullets topic="Blog Format" points={["Common generic format to post articles and updates from a single source."]}/>
 		<Bullets topic="Forums format" points={["Common generic format to post articles and media from a community."]}/>
 		</div>
-		</div>
-
-
 		);
 	}
 }
+
+class Next extends React.Component {
+	render() {
+		var topic = {
+			float:"left",
+			marginRight:"100px"
+		};
+		var link = {
+			float:"left",
+			width:"400px"	
+		};
+		return (
+		<div id="next">
+			<div className="fit">
+			<p> List</p>
+			<a href="https://Bullvalene.com">Bullvalene.com</a>
+			</div>
+			<div className="fit">
+			<p>Forum</p>
+			<a href="https://Usipun.com">Usipun.com</a>
+			</div>
+			<div className="fit">
+			<p>shop</p>
+			<a href="https://snapchaching.com">snapchaching.com</a>
+			</div>
+		</div>
+		);
+	}
+}
+
+
 class Pages extends React.Component {
 	render() {
 	return (
-	<div id="page">
-		<div className="fit">
-		<FantasticEntry contents={<img id="pic" width="300px" src="src/placeholder.png"/>}/>
+	<div>
+		<div id="card">
+			<div className="fit">
+			<FantasticEntry contents={<img id="pic" width="50px" height="50px" src="src/placeholder.png"/>}/>
+			</div>
+			<div className="fit">
+			<FantasticEntry contents={<img id="logo" width="50px" height="50px" src="src/phlogo.jpg"/>}/>
+			</div>
+			<div className="fit">
+			<FantasticEntry contents={<WelcomeText/>}/>
+			</div>
+			<div className="fit">
+			<FantasticEntry contents={<Media/>}/>
+			</div>
+			<div className="fit" id="topGap">
+			</div>
 		</div>
-		<div className="fit">
-		<FantasticEntry contents={<img id="logo" width="372px" src="src/phlogo.jpg"/>}/>
-		</div>
-		<div className="fit">
-		<FantasticEntry contents={<WelcomeText/>}/>
-		</div>
-		<div className="fit" id="topGap">
-		</div>
-		<div className="full">
-		<Body/>
-		</div>
-
 	</div>);
 	}
 }
-class Dragbird extends React.Component {
+
+class Content extends React.Component {
 	render() {
 		return (
-		<div>
-			<p></p>
-		</div>
-		);
+		<div id="contentContainer">
+			<Brief/>
+			<Next/>
+		</div>);
 	}
 }
-ReactDOM.render (<Pages/>, document.getElementById("Intro"));
-ReactDOM.render (<Dragbird/>,document.getElementById("Dragbird"));
+ReactDOM.render(<Pages/>, document.getElementById("Intro"));
+ReactDOM.render(<Content/>, document.getElementById("Content"));
+/*
+			<div id="s2" className="slide">
+				<div className="page">
+				<Next/>
+				</div>
+			</div>
+			*/
